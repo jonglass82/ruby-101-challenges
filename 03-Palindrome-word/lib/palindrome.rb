116b925060f408a6bc86f9c1ab2e47da7e5659ff
Jon Glass <jonglass82@gmail.com>
2018-11-29 15:05:39 -0600

    challenge complete, all tests passed

--- 03-Palindrome-word/lib/palindrome.rb
+++ 03-Palindrome-word/lib/palindrome.rb
@@ -1,7 +1,14 @@
 def palindrome?(word)
   # TODO: return true/false depending if the *word* is a palindrome
+    word = word.downcase
+    
+    if word == word.reverse
+        true
+    else
+        false
+    end
 
 end
 
 # To see the result of this method you can uncomment the line below:
-# puts palindrome?("Stats")
+ puts palindrome?("Stats")
