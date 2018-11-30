b6dd730ab39ae932c2caa051aef5c489cabb1cde
Jon Glass <jonglass82@gmail.com>
2018-11-30 11:18:22 -0600

    challenge complete, all tests passed

--- 08-Can-you-vote/lib/can_you_vote.rb
+++ 08-Can-you-vote/lib/can_you_vote.rb
@@ -1,7 +1,16 @@
 def can_you_vote?(age)
   # TODO: return true/false depending on the *age* given
+    if age.integer?
+       if age < 18
+           return false
+       else
+           return true
+       end
+    else
+        "NAN"
+    end
 
 end
 
 # To see the result of this method you can uncomment the line below:
-# p can_you_vote?(18)
+ p can_you_vote?(18)
