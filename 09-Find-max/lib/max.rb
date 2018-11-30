d78c6d163f406acfa784b05f59bc507f88f158d9
Jon Glass <jonglass82@gmail.com>
2018-11-30 11:26:05 -0600

    challenge complete, all tests passed

--- 09-Find-max/lib/max.rb
+++ 09-Find-max/lib/max.rb
@@ -1,7 +1,12 @@
 def max(a, b)
   # TODO: return the highest number of a and b
+    if a>b
+        a
+    else
+        b
+    end
 
 end
 
 # To see the result of this method you can uncomment the line below:
-# p max(2, 3)
+ p max(2, 3)
