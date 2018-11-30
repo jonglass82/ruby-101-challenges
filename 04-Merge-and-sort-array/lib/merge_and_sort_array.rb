1f3d5003c612a28cb5afb21d835eb7b5d1b1baa8
Jon Glass <jonglass82@gmail.com>
2018-11-30 09:29:34 -0600

    challenge complete, all tests pass

--- 04-Merge-and-sort-array/lib/merge_and_sort_array.rb
+++ 04-Merge-and-sort-array/lib/merge_and_sort_array.rb
@@ -1,8 +1,11 @@
 def merge_and_sort_array(array_one, array_two)
   # TODO: return a the given array sorted alphabetically
+    array_one.concat(array_two).sort
+    
+    
 
 end
 
 # To see the result of this method you can uncomment the line below:
-# p merge_and_sort_array(['B', 'C'], ['A', 'D'])
+ p merge_and_sort_array(['B', 'C'], ['A', 'D'])
 
