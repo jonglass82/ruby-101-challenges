8ed7267d236acc7bcc1f86434c0ed77b52115278
Jon Glass <jonglass82@gmail.com>
2018-11-30 11:11:43 -0600

    challenege complete, all tests passed

--- 07-odd-or-even/lib/odd_or_even.rb
+++ 07-odd-or-even/lib/odd_or_even.rb
@@ -1,7 +1,17 @@
 def odd_or_even(number)
   # TODO: return "odd"/"even" depending on the *number*
+    if number.integer?
+        if number.odd?
+            "odd"
+        else
+            "even"
+        end
+    
+    else
+        "NAN"
+    end
 
 end
 
 # To see the result of this method you can uncomment the line below:
-# p odd_or_even(2)
+ p odd_or_even(2)
